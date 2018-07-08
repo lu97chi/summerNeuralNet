49cce1776f85d3f30467e4910e14dd5b5ad16188
lu97chi <lu97is@gmail.com>
2018-07-08 16:22:26 -0500

    make

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import numpy as np
-from flask import render_template,Flask, abort, jsonify, request,Response, json
-from sqlalchemy import create_engine
-
+from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
-
 
 @app.route('/')
 def homepage():
-	return render_template("index2.html", title = 'Prediction Module Test1')
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
